4da87bf17d5b6a2af5bd606949c156b71cbed02c
Konstantin <konstantinleonov89@gmail.com>
2019-04-21 13:21:14 +0300

    Add hw01maven/pom.xml

---
+++ hw01maven/src/main/java/HelloOtus.java
@@ -0,0 +1,14 @@
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        List<String> names = Lists.newArrayList("John", "Jane", "Adam", "Tom");
+        String result = Joiner.on(",").join(names);
+
+        System.out.println(result + " = John,Jane,Adam,Tom");
+    }
+}
