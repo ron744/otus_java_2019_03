6ab5a308d0b5ecd12767065e5250e89ed200675f
Konstantin <konstantinleonov89@gmail.com>
2019-05-18 18:32:27 +0300

    new

--- hw03Annotations/src/main/java/annotations/AnnotationsTest.java
+++ hw03Annotations/src/main/java/annotations/AnnotationsTest.java
@@ -3,23 +3,24 @@ package annotations;
 public class AnnotationsTest {
 
     @BeforeEach
-    public static void before(){ System.out.println("before test"); }
+    public void before(){
+        System.out.println("before test"); }
 
     @Test
-    public static void test1() { System.out.println("test1"); }
+    public void test1() { System.out.println("test1"); }
 
     @Test
-    public static void test2(){
+    public void test2(){
         System.out.println("test2");
     }
 
     @Test
-    public static void test3(){
+    public void test3(){
         System.out.println("test3");
     }
 
     @AfterEach
-    public static void after(){
+    public void after(){
         System.out.println("after test");
     }
 }
--- hw03Annotations/src/main/java/annotations/TestRunner.java
+++ hw03Annotations/src/main/java/annotations/TestRunner.java
@@ -2,47 +2,43 @@ package annotations;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.List;
 
 public class TestRunner {
 
-    public static void main(String[] args){
-        run(AnnotationsTest.class);
-    }
-
-    private static void run(Class<?> testClass){
+    public static void run(Class<?> testClass){
 
         Method[] testMethods = testClass.getDeclaredMethods();
-        for(Method testMethod : testMethods){
-            if(testMethod.isAnnotationPresent(Test.class)){
-                try {
-                    Constructor<?> constructor = testClass.getConstructor();
-                    Object object = constructor.newInstance();
-                    Method[] beforeMethods = testClass.getDeclaredMethods();
-                    boolean flag = true;
-                    for(Method beforeMethod : beforeMethods){
-                        if(beforeMethod.isAnnotationPresent(BeforeEach.class)){
-                            try {
-                                beforeMethod.invoke(object);
-                            }catch (Exception e){
-                                flag = false;
-                            }
-                        }
-                    }
-                    if (flag)
-                        testMethod.invoke(object);
 
-                    Method[] afterMethods = testClass.getDeclaredMethods();
-                    for(Method afterMethod : afterMethods){
-                        if(afterMethod.isAnnotationPresent(AfterEach.class)){
-                            try {
-                                afterMethod.invoke(object);
-                            }catch (Exception e){
-                            }
-                        }
-                    }
+        List<Method> methodsTest = new PrepareTest().prepare(testMethods, Test.class);
+        List<Method> methodsBefore = new PrepareTest().prepare(testMethods, BeforeEach.class);
+        List<Method> methodsAfter = new PrepareTest().prepare(testMethods, AfterEach.class);
 
-                }catch (Exception e){
+        for (Method testMethod : methodsTest){
+            try {
+                Constructor<?> constructor = testClass.getConstructor();
+                Object object = constructor.newInstance();
+                boolean flag = true;
+                for (Method beforeMethod : methodsBefore){
+                    try{
+                        beforeMethod.invoke(object);
+                    }catch (Exception e){
+                        flag = false;
+                    }
                 }
+                if (flag)
+                    testMethod.invoke(object);
+
+                for (Method afterMethod : methodsAfter){
+                    try{
+                        afterMethod.invoke(object);
+                    }catch (Exception e){
+
+                    }
+                }
+
+            }catch(Exception e){
+
             }
         }
     }
